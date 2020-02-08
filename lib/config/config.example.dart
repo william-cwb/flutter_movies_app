b97708aaed66e2d038685acdccea4eba739c7ca0
William David da Silva <william.silva@wises.com.br>
2020-02-07 21:33:59 -0300

    config file

---
+++ lib/config/config.example.dart
@@ -0,0 +1,5 @@
+class Config {
+  static final String base_url_movie = 'https://api.themoviedb.org/3/';
+  static final String base_url_img = 'https://image.tmdb.org/t/p/w500';
+  static final String api_key = "YOU API KEY HERE";
+}
